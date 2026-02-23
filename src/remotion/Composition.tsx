6a0a2206e3a81d04eda524a9e31bcd84206141ad
Pascal Pothmann <pascal.pothmann@gmail.com>
2026-02-23 21:43:39 +0100

    refactor: Replace `OffthreadVideo` with `Html5Video` and update video trimming props.

--- src/remotion/Composition.tsx
+++ src/remotion/Composition.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AbsoluteFill, OffthreadVideo, Series } from "remotion";
+import { AbsoluteFill, Series, Html5Video } from "remotion";
 import type { CompositionProps } from "../types";
 
 export const VideoComposition: React.FC<CompositionProps> = ({ clips, fps }) => {
@@ -14,10 +14,10 @@ export const VideoComposition: React.FC<CompositionProps> = ({ clips, fps }) => 
           return (
             <Series.Sequence key={index} durationInFrames={durationInFrames}>
               <AbsoluteFill>
-                <OffthreadVideo
+                <Html5Video
                   src={clip.url}
-                  startFrom={startFrame}
-                  endAt={endFrame}
+                  trimBefore={startFrame}
+                  trimAfter={endFrame}
                   style={{ width: "100%", height: "100%" }}
                 />
               </AbsoluteFill>
